ef99dbcad0c280819a609ab1c805b0d8cfacfa77
MACDOUGLAS\Thomas <tholebris@gmail.com>
2021-04-07 11:14:39 +0200

    Fini l'authentification

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 const userRoutes = require('./routes/user.routes');
 
 //? DB Connection
@@ -11,8 +14,16 @@ mongoose.connect('mongodb://localhost/jwt_auth', {
     console.log('connected to MongDB');
 });
 
+//? Instanciation de mon serveur & middlewares
 //TODO: Créer un fichier .env qui contient les variables d'environnement comme l'Url de connexion
 const app = express();
+
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:8000', 'http://localhost:"3000', 'http://localhost:4200']
+}))
+
 app.use(express.json());
 
 //? Routes
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,11 @@
+//? Récupération des router de express afin d'assurer que les routes soit servies sur des url qu'on définit
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const User = require('../models/user.model');
 
-router.get('/', (req,res)=>{
-    res.send('Hello from user');
-});
+//? La dépendance jsonwebtoken qui permet de créer des json web token, mais aussi de les utiliser
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user.model');
 
 router.post('/register', async (req,res)=>{
 
@@ -24,4 +25,66 @@ router.post('/register', async (req,res)=>{
     res.send(data);
 })
 
+//? Rajouter la route /login, qui sert à se connecter sur notre server
+router.post('/login', async (req,res)=>{
+    
+    //? 3 étapes: 
+    //?1. vérifier qu'un utilisateur possède cette adresse
+    const user = await User.findOne({email:req.body.email});
+    if(!user){
+        return res.status(404).send({
+            message:'User not found'
+        })
+    }
+
+    //? 2. vérifier que le mdp est valide
+    if (!await bcrypt.compare(req.body.password, user.password)) {
+        return res.status(404).send({
+            message:'Invalide credentials'
+        })
+    }
+    //? 3. créer un token de session
+    const token = jwt.sign({_id:user._id}, "secret");
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000  // 1 day in ms
+    })
+
+    res.send({
+        message:'Authentification success'
+    });
+})
+
+//? La route / va servir de route qui récupère mes infos de l'utilisateur authentifié.
+router.get('/', async (req,res)=>{
+    try {
+        
+        const cookie = req.cookies['jwt']
+
+        const claims = await jwt.verify(cookie, 'secret');
+        if(!claims) {
+            return res.status(401).send({
+                message: 'Not authenticated'
+            })
+        }
+        const user = await User.findOne({_id:claims._id});
+        const {password, ...data} = await user.toJSON(); 
+
+        res.send(data);
+
+    } catch (error) {
+        return res.status(401).send({
+            message: 'Not authenticated'
+        })
+    }
+})
+
+router.post('/logout', (req,res)=>{
+    res.cookie('jwt', '', {maxAge:0});
+
+    res.send({
+        message:'Successfully logged out'
+    })
+})
 module.exports = router;
